f3731de52ee7e664e68448b6cc226167729ef607
newt1409 <newt1409@hotmail.com>
2022-01-22 10:59:31 -0600

    logstash working

---
+++ Dockerfiles/logstash.Dockerfile
@@ -0,0 +1,98 @@
+FROM opensearchproject/logstash-oss-with-opensearch-output-plugin:7.16.2
+
+# Copyright (c) 2022 Battelle Energy Alliance, LLC.  All rights reserved.
+
+LABEL maintainer="malcolm@inl.gov"
+LABEL org.opencontainers.image.authors='malcolm@inl.gov'
+LABEL org.opencontainers.image.url='https://github.com/cisagov/Malcolm'
+LABEL org.opencontainers.image.documentation='https://github.com/cisagov/Malcolm/blob/main/README.md'
+LABEL org.opencontainers.image.source='https://github.com/cisagov/Malcolm'
+LABEL org.opencontainers.image.vendor='Cybersecurity and Infrastructure Security Agency'
+LABEL org.opencontainers.image.title='malcolmnetsec/logstash-oss'
+LABEL org.opencontainers.image.description='Malcolm container providing Logstash (the Apache-licensed variant)'
+
+ARG DEFAULT_UID=1000
+ARG DEFAULT_GID=1000
+ENV DEFAULT_UID $DEFAULT_UID
+ENV DEFAULT_GID $DEFAULT_GID
+ENV PUSER "logstash"
+ENV PGROUP "logstash"
+ENV PUSER_PRIV_DROP true
+
+ENV TERM xterm
+
+ARG LOGSTASH_ENRICHMENT_PIPELINE=enrichment
+ARG LOGSTASH_PARSE_PIPELINE_ADDRESSES=zeek-parse
+ARG LOGSTASH_OPENSEARCH_PIPELINE_ADDRESS_INTERNAL=internal-os
+ARG LOGSTASH_OPENSEARCH_PIPELINE_ADDRESS_EXTERNAL=external-os
+ARG LOGSTASH_OPENSEARCH_OUTPUT_PIPELINE_ADDRESSES=internal-os,external-os
+
+ENV LOGSTASH_ENRICHMENT_PIPELINE $LOGSTASH_ENRICHMENT_PIPELINE
+ENV LOGSTASH_PARSE_PIPELINE_ADDRESSES $LOGSTASH_PARSE_PIPELINE_ADDRESSES
+ENV LOGSTASH_OPENSEARCH_PIPELINE_ADDRESS_INTERNAL $LOGSTASH_OPENSEARCH_PIPELINE_ADDRESS_INTERNAL
+ENV LOGSTASH_OPENSEARCH_PIPELINE_ADDRESS_EXTERNAL $LOGSTASH_OPENSEARCH_PIPELINE_ADDRESS_EXTERNAL
+ENV LOGSTASH_OPENSEARCH_OUTPUT_PIPELINE_ADDRESSES $LOGSTASH_OPENSEARCH_OUTPUT_PIPELINE_ADDRESSES
+ENV JAVA_HOME=/usr/share/logstash/jdk
+
+USER root
+
+RUN yum install -y epel-release && \
+    yum update -y && \
+    yum install -y curl gettext python3-setuptools python3-pip python3-requests openssl && \
+    yum clean all && \
+    pip3 install ipaddress supervisor manuf pyyaml && \
+    logstash-plugin install logstash-filter-translate logstash-filter-cidr logstash-filter-dns \
+                            logstash-filter-json logstash-filter-prune logstash-filter-http \
+                            logstash-filter-grok logstash-filter-geoip logstash-filter-uuid \
+                            logstash-filter-kv logstash-filter-mutate logstash-filter-dissect \
+                            logstash-input-beats logstash-output-elasticsearch && \
+    rm -rf /root/.cache /root/.gem /root/.bundle
+
+ADD shared/bin/docker-uid-gid-setup.sh /usr/local/bin/
+ADD shared/bin/manuf-oui-parse.py /usr/local/bin/
+ADD shared/bin/jdk-cacerts-auto-import.sh /usr/local/bin/
+ADD logstash/maps/*.yaml /etc/
+ADD logstash/config/log4j2.properties /usr/share/logstash/config/
+ADD logstash/config/logstash.yml /usr/share/logstash/config/
+ADD logstash/pipelines/ /usr/share/logstash/malcolm-pipelines/
+ADD logstash/ruby/ /usr/share/logstash/malcolm-ruby/
+ADD logstash/scripts /usr/local/bin/
+ADD logstash/supervisord.conf /etc/supervisord.conf
+
+RUN bash -c "chmod --silent 755 /usr/local/bin/*.sh /usr/local/bin/*.py || true" && \
+    usermod -a -G tty ${PUSER} && \
+    rm -f /usr/share/logstash/pipeline/logstash.conf && \
+    rmdir /usr/share/logstash/pipeline && \
+    mkdir /logstash-persistent-queue && \
+    chown --silent -R ${PUSER}:root /usr/share/logstash/malcolm-pipelines /logstash-persistent-queue && \
+    echo "Retrieving and parsing Wireshark manufacturer database..." && \
+    python3 /usr/local/bin/manuf-oui-parse.py -o /etc/vendor_macs.yaml && \
+    echo "Retrieving JA3 fingerprint lists..." && \
+    python3 /usr/local/bin/ja3_build_list.py -o /etc/ja3.yaml && \
+    echo "Complete."
+
+# As the keystore is encapsulated in logstash, this isn't really necessary. It's included
+# here just to suppress the prompt when creating the keystore. If you're concerned about it
+# you could change or remove this from the Dockerfile, and/or override it with your
+# own envrionment variable at runtime.
+ENV LOGSTASH_KEYSTORE_PASS "a410a267b1404c949284dee25518a917"
+
+VOLUME ["/logstash-persistent-queue"]
+
+EXPOSE 5044
+EXPOSE 9001
+EXPOSE 9600
+
+ENTRYPOINT ["/usr/local/bin/docker-uid-gid-setup.sh"]
+
+CMD ["/usr/local/bin/supervisord", "-c", "/etc/supervisord.conf", "-n"]
+
+
+# to be populated at build-time:
+ARG BUILD_DATE
+ARG MALCOLM_VERSION
+ARG VCS_REVISION
+
+LABEL org.opencontainers.image.created=$BUILD_DATE
+LABEL org.opencontainers.image.version=$MALCOLM_VERSION
+LABEL org.opencontainers.image.revision=$VCS_REVISION
---
+++ logstash/scripts/logstash-start.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2022 Battelle Energy Alliance, LLC.  All rights reserved.
+
+set -e
+
+# if any pipelines are volume-mounted inside this docker container, they should belong to subdirectories under this path
+HOST_PIPELINES_DIR="/usr/share/logstash/malcolm-pipelines.available"
+
+# runtime pipelines parent directory
+export PIPELINES_DIR="/usr/share/logstash/malcolm-pipelines"
+
+# runtime pipeliens configuration file
+export PIPELINES_CFG="/usr/share/logstash/config/pipelines.yml"
+
+# for each pipeline in /usr/share/logstash/malcolm-pipelines, append the contents of this file to the dynamically-generated
+# pipeline section in pipelines.yml (then delete 00_config.conf before starting)
+export PIPELINE_EXTRA_CONF_FILE="00_config.conf"
+
+# files defining IP->host and MAC->host mapping
+INPUT_CIDR_MAP="/usr/share/logstash/config/cidr-map.txt"
+INPUT_HOST_MAP="/usr/share/logstash/config/host-map.txt"
+INPUT_MIXED_MAP="/usr/share/logstash/config/net-map.json"
+
+# the name of the enrichment pipeline subdirectory under $PIPELINES_DIR
+ENRICHMENT_PIPELINE=${LOGSTASH_ENRICHMENT_PIPELINE:-"enrichment"}
+
+# the name of the pipeline(s) to which input will send logs for parsing (comma-separated list, no quotes)
+PARSE_PIPELINE_ADDRESSES=${LOGSTASH_PARSE_PIPELINE_ADDRESSES:-"zeek-parse"}
+
+# pipeline addresses for forwarding from Logstash to OpenSearch (both "internal" and "external" pipelines)
+export OPENSEARCH_PIPELINE_ADDRESS_INTERNAL=${LOGSTASH_OPENSEARCH_PIPELINE_ADDRESS_INTERNAL:-"internal-os"}
+export OPENSEARCH_PIPELINE_ADDRESS_EXTERNAL=${LOGSTASH_OPENSEARCH_PIPELINE_ADDRESS_EXTERNAL:-"external-os"}
+OPENSEARCH_OUTPUT_PIPELINE_ADDRESSES=${LOGSTASH_OPENSEARCH_OUTPUT_PIPELINE_ADDRESSES:-"$OPENSEARCH_PIPELINE_ADDRESS_INTERNAL,$OPENSEARCH_PIPELINE_ADDRESS_EXTERNAL"}
+
+# ip-to-segment-logstash.py translate $INPUT_CIDR_MAP, $INPUT_HOST_MAP, $INPUT_MIXED_MAP into this logstash filter file
+NETWORK_MAP_OUTPUT_FILTER="$PIPELINES_DIR"/"$ENRICHMENT_PIPELINE"/16_host_segment_filters.conf
+
+####################################################################################################################
+
+# copy over pipeline filters from host-mapped volumes (if any) into their final resting places
+find "$HOST_PIPELINES_DIR" -mindepth 1 -maxdepth 1 -type d -print0 2>/dev/null | sort -z | \
+  xargs -0 -n 1 -I '{}' bash -c '
+  PIPELINE_NAME="$(basename "{}")"
+  PIPELINES_DEST_DIR="$PIPELINES_DIR"/"$PIPELINE_NAME"
+  mkdir -p "$PIPELINES_DEST_DIR"
+  cp -f "{}"/* "$PIPELINES_DEST_DIR"/
+'
+
+# dynamically generate final pipelines.yml configuration file from all of the pipeline directories
+> "$PIPELINES_CFG"
+find "$PIPELINES_DIR" -mindepth 1 -maxdepth 1 -type d -print0 2>/dev/null | sort -z | \
+  xargs -0 -n 1 -I '{}' bash -c '
+  PIPELINE_NAME="$(basename "{}")"
+  PIPELINE_ADDRESS_NAME="$(cat "{}"/*.conf | sed -e "s/:[\}]*.*\(}\)/\1/" | envsubst | grep -P "\baddress\s*=>" | awk "{print \$3}" | sed "s/[\"'']//g" | head -n 1)"
+  if [[ -n "$OS_EXTERNAL_HOSTS" ]] || [[ "$PIPELINE_ADDRESS_NAME" != "$OPENSEARCH_PIPELINE_ADDRESS_EXTERNAL" ]]; then
+    echo "- pipeline.id: malcolm-$PIPELINE_NAME"       >> "$PIPELINES_CFG"
+    echo "  path.config: "{}""                         >> "$PIPELINES_CFG"
+    echo "  pipeline.ecs_compatibility: disabled"      >> "$PIPELINES_CFG"
+    cat "{}"/"$PIPELINE_EXTRA_CONF_FILE" 2>/dev/null   >> "$PIPELINES_CFG"
+    rm -f "{}"/"$PIPELINE_EXTRA_CONF_FILE"
+    echo                                               >> "$PIPELINES_CFG"
+    echo                                               >> "$PIPELINES_CFG"
+  fi
+'
+
+# create filters for network segment and host mapping in the enrichment directory
+rm -f "$NETWORK_MAP_OUTPUT_FILTER"
+/usr/local/bin/ip-to-segment-logstash.py --mixed "$INPUT_MIXED_MAP" --segment "$INPUT_CIDR_MAP" --host "$INPUT_HOST_MAP" -o "$NETWORK_MAP_OUTPUT_FILTER"
+
+if [[ -z "$OS_EXTERNAL_HOSTS" ]]; then
+  # external ES host destination is not specified, remove external destination from enrichment pipeline output
+  OPENSEARCH_OUTPUT_PIPELINE_ADDRESSES="$(echo "$OPENSEARCH_OUTPUT_PIPELINE_ADDRESSES" | sed "s/,[[:blank:]]*$OPENSEARCH_PIPELINE_ADDRESS_EXTERNAL//")"
+fi
+
+# insert quotes around the OpenSearch parsing and output pipeline list
+MALCOLM_PARSE_PIPELINE_ADDRESSES=$(printf '"%s"\n' "${PARSE_PIPELINE_ADDRESSES//,/\",\"}")
+MALCOLM_OPENSEARCH_OUTPUT_PIPELINES=$(printf '"%s"\n' "${OPENSEARCH_OUTPUT_PIPELINE_ADDRESSES//,/\",\"}")
+
+# do a manual global replace on these particular values in the config files, as Logstash doesn't like the environment variables with quotes in them
+find "$PIPELINES_DIR" -type f -name "*.conf" -exec sed -i "s/_MALCOLM_OPENSEARCH_OUTPUT_PIPELINES_/${MALCOLM_OPENSEARCH_OUTPUT_PIPELINES}/g" "{}" \; 2>/dev/null
+find "$PIPELINES_DIR" -type f -name "*.conf" -exec sed -i "s/_MALCOLM_PARSE_PIPELINE_ADDRESSES_/${MALCOLM_PARSE_PIPELINE_ADDRESSES}/g" "{}" \; 2>/dev/null
+
+# import trusted CA certificates if necessary
+/usr/local/bin/jdk-cacerts-auto-import.sh || true
+
+# start logstash (adapted from docker-entrypoint)
+env2yaml /usr/share/logstash/config/logstash.yml
+export LS_JAVA_OPTS="-Dls.cgroup.cpuacct.path.override=/ -Dls.cgroup.cpu.path.override=/ $LS_JAVA_OPTS"
+exec logstash
